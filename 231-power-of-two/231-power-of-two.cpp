4081e6f42b05aee965523fc1f328dae3f167436d
KISHAN KUMAR <81502923+kishan779@users.noreply.github.com>
2022-09-16 02:54:11 +0530

    Time: 0 ms (100.00%), Space: 5.7 MB (88.99%) - LeetHub

--- 231-power-of-two/231-power-of-two.cpp
+++ 231-power-of-two/231-power-of-two.cpp
@@ -2,21 +2,14 @@ class Solution {
 public:
     bool isPowerOfTwo(int n) {
         
-        
-        int ans =1;
+    
         for(int i=0; i<=30; i++){
             
-            // cout<<"ans"<<ans<<endl;
+            int ans = pow(2,i);
             
             if (ans == n){
                 return true;
             }
-            
-            if(ans <INT_MAX/2)
-            ans = ans *2; // purana waala ans baar baar multiply hota rahega 2 se
-            // ans * 2 - to ye 2 power 31 waala bhi include ho jaayega ...so condition laagao.
-            
-            
         }
         return false;
     }
