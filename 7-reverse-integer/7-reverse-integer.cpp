12a29bb4415b25caeb1cf30f9f7c3eb316a8e9da
KISHAN KUMAR <81502923+kishan779@users.noreply.github.com>
2022-09-16 02:05:19 +0530

    Time: 7 ms (19.85%), Space: 5.9 MB (75.69%) - LeetHub

---
+++ 7-reverse-integer/7-reverse-integer.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    
+   int  ans = 0;
+    int reverse(int n) {
+        
+        while(n!=0){
+            int digit = n %10;
+            if ((ans > INT_MAX/10) || (ans < INT_MIN/10) ){
+                return 0;
+            }else {
+                ans = (ans*10)+ digit;
+            n = n/10;
+            }
+            
+        }   
+         return ans;
+    }
+    
+};
+
+
+
+
+
+
