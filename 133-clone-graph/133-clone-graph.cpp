ade6435f7acb5ebf8b024a374e1c1c5c1650c8f8
KISHAN KUMAR <81502923+kishan779@users.noreply.github.com>
2022-10-09 01:03:10 +0530

    Time: 18 ms (10.82%), Space: 9.1 MB (10.94%) - LeetHub

--- 133-clone-graph/133-clone-graph.cpp
+++ 133-clone-graph/133-clone-graph.cpp
@@ -64,10 +64,3 @@ public:
 };
 
 
-static int fastio = []() {
-    //#define endl '\n'
-    std::ios::sync_with_stdio(false);
-    //std::cin.tie(NULL);
-    //std::cout.tie(0);
-    return 0;
-}();
