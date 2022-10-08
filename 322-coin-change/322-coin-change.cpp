74727d59038de2493e7e4535aefe4a73653f9642
KISHAN KUMAR <81502923+kishan779@users.noreply.github.com>
2022-10-09 01:06:13 +0530

    Time: 80 ms (91.92%), Space: 10.6 MB (89.07%) - LeetHub

---
+++ 322-coin-change/322-coin-change.cpp
@@ -0,0 +1,52 @@
+//C++ Code
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        int n = coins.size();
+        int dp[n+1][amount+1];
+        
+        for(int i=0;i<=n;++i)
+        {
+            for(int j=0;j<=amount;++j)
+            {
+                if(j==0)
+                    dp[i][j] = 0;
+                else if(i==0)
+                    dp[i][j] = 1e5;
+                else if(coins[i-1]>j)
+                    dp[i][j] = dp[i-1][j];
+                else
+                    dp[i][j] = min(1 + dp[i][j-coins[i-1]], dp[i-1][j]);
+            }
+        }
+        return dp[n][amount]>1e4 ? -1:dp[n][amount];
+    }
+};
+
+/*
+//Java
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int n = coins.length;
+        
+        int dp[][] = new int[n+1][amount+1];
+        
+        for(int i=0;i<=n;++i)
+        {
+            for(int j=0;j<=amount;++j)
+            {
+                if(j==0)
+                    dp[i][j] = 0;
+                else if(i==0)
+                    dp[i][j] = 100000;
+                else if(coins[i-1]>j)
+                    dp[i][j] = dp[i-1][j];
+                else
+                    dp[i][j] = Math.min(1 + dp[i][j-coins[i-1]], dp[i-1][j]);
+            }
+        }
+        return dp[n][amount]>1e4 ? -1:dp[n][amount];
+    }
+}
+
+*/
