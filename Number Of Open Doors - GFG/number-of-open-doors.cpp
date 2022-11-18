e81c26860fc6e0f200fbac519539f27bd50846fe
KISHAN KUMAR <81502923+kishan779@users.noreply.github.com>
2022-11-18 14:51:57 +0530

    Added solution - LeetHub

---
+++ Number Of Open Doors - GFG/number-of-open-doors.cpp
@@ -0,0 +1,30 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int noOfOpenDoors(long long N) {
+        // code here
+        
+        double s= sqrt(N+1)-1;
+        return ceil(s);
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long N;
+        
+        cin>>N;
+
+        Solution ob;
+        cout << ob.noOfOpenDoors(N) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
