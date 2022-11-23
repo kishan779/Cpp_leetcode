76b8161b423324864cad980797d2d6ae29a36243
KISHAN KUMAR <81502923+kishan779@users.noreply.github.com>
2022-11-23 11:04:17 +0530

    Added solution - LeetHub

---
+++ Maximum Sum LCM - GFG/maximum-sum-lcm.cpp
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+//Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+class Solution {
+  public:
+    long long int maxSumLCM(int n) 
+    {
+        // code here
+        long long ans=0;
+        for(int i=1; i<=sqrt(n); i++){
+            if(n % i == 0){
+                ans += i;
+                long long m = n/i;
+                if(m != i)  ans+= m;
+            }
+            
+            
+        }
+        
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int  n;
+        cin >> n ;
+        Solution ob;
+        cout<<ob.maxSumLCM(n)<<endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
