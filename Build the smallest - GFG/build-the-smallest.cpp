de14c147c7e1e209fffe6fc21a2d1d50cd69012b
KISHAN KUMAR <81502923+kishan779@users.noreply.github.com>
2022-12-10 22:55:18 +0530

    Added solution - LeetHub

---
+++ Build the smallest - GFG/build-the-smallest.cpp
@@ -0,0 +1,79 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+string buildLowestNumber(string num, int k);
+// Driver program to test above function
+int main()
+{
+    int t,n;
+    string str;
+    cin>>t;
+    while(t--)
+    {
+    cin>>n;
+    cin>>str;
+    cout << buildLowestNumber(str, n)<<endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
+
+
+string buildLowestNumber(string num, int k) {
+
+    stack<char> st;
+
+    st.push(num[0]);
+
+    int n = num.length();
+
+    for(int itr = 1; itr < n;++itr) {
+
+        while(!st.empty() && st.top() > num[itr] && k > 0) {
+
+            st.pop();
+
+            --k;
+
+        }
+
+        st.push(num[itr]);
+
+    }
+
+    while(k > 0) {
+
+        st.pop();
+
+        --k;
+
+    }
+
+    string ans = "";
+
+    while(!st.empty()) {
+
+        ans=st.top()+ans;
+
+        st.pop();
+
+    }
+
+    int leadingIndex = -1;
+
+    for(int itr = 0; itr < ans.length(); ++itr) {
+
+        if(ans[itr] != '0') break;
+
+        leadingIndex = itr;
+
+    }
+
+    if(leadingIndex == ans.length()-1) return "0";
+
+    if(leadingIndex >= 0) return ans.substr(leadingIndex+1);
+
+    return ans;
+
+}
