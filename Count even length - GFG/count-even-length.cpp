cd47fbe652ce691eba575d3131e3c606a10fb7be
KISHAN KUMAR <81502923+kishan779@users.noreply.github.com>
2023-01-01 13:31:32 +0530

    Added solution - LeetHub

---
+++ Count even length - GFG/count-even-length.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+
+class Solution{
+	public:
+	
+int inv(int n, int m, int mod=1000000007){
+      if(m==0) return 1;
+     if(m&1) return (n*1ll*inv(n, m-1))%mod;
+      int tmp=inv(n, m/2);
+       return (tmp*1ll*tmp)%mod;
+    }
+          int compute_value(int n)
+        {
+          // Code here
+       int m = 1000000007;
+      long long fact2N=1, factN;
+
+
+     for(int i=1; i<=2*n; i++){
+       fact2N *= i;
+        fact2N %= m;
+    if(i==n) factN=fact2N; } 
+         
+         factN = (factN*factN)%m;
+
+
+      return (fact2N * 1ll * inv(factN, m-2))%m;
+        }
+	
+};
+
+
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n;
+		cin >> n;
+		Solution ob;
+		int ans = ob.compute_value(n);
+		cout << ans <<"\n";
+	}
+	return 0;
+}
+// } Driver Code Ends
